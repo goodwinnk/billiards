b80985a815fe3cabae26fefe85b8df873b9477d0
FadeevaKate <rediska0123@gmail.com>
2020-03-28 02:23:35 +0300

    add video compresser

---
+++ test_video_compresser.py
@@ -0,0 +1,18 @@
+import pytest, os
+from video_compresser import compress
+
+
+def test_compress():
+    tests = [
+        (
+            'video/compress_test.mp4',
+            'video/compress_test_output.mp4',
+            50,
+            2000,
+        ),
+    ]
+    for inp_file, outp_file, bitrate, high_threshold_sz in tests:
+        compress(inp_file, outp_file, bitrate)
+        sz = os.path.getsize(outp_file)
+        os.remove(outp_file)
+        assert sz <= high_threshold_sz
---
+++ video_compresser.py
@@ -0,0 +1,34 @@
+import ffmpy
+import argparse
+import os
+
+
+# Compresses video at input_filename using bitrate number (kbits/sec)
+# and saves it to the output_filename.
+# Deletes audio if delete_audio flag is set.
+def compress(input_filename, output_filename, bitrate, delete_audio=True):
+    inp_sz = os.path.getsize(input_filename)
+    flags = '-y -strict -2 -b:v {}k'.format(bitrate)
+    if delete_audio:
+        flags += ' -an'
+    inp = {input_filename: None}
+    outp = {output_filename: flags}
+    ff = ffmpy.FFmpeg(inputs=inp, outputs=outp)
+    ff.run()
+    out_sz = os.path.getsize(output_filename)
+    print('Total {0:.3f}% lost'.format(100 * (inp_sz - out_sz) / inp_sz))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''
+		Compress input video with the given bitrate and save the result in output file.
+		The estimated video size can be approximately computed as (bitrate) * (length in seconds) kB.
+	''')
+    parser.add_argument('-i', metavar='input', required=True,
+                        help='input file name')
+    parser.add_argument('-o', metavar='output', required=True,
+                        help='output file name')
+    parser.add_argument('-b', metavar='bitrate', type=int, default=400,
+                        help='bitrate to compress input to (in kbits/sec)')
+    args = parser.parse_args()
+    compress(args.i, args.o, args.b)

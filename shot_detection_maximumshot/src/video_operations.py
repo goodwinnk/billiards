443c1bc8b6c31779428708d6c42da7a1220b3a08
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2020-04-11 23:53:23 +0300

    modify exp3

--- shot_detection_maximumshot/src/video_operations.py
+++ shot_detection_maximumshot/src/video_operations.py
@@ -1,8 +1,10 @@
 import numpy as np
 import cv2
+import typing
+import numpy as np
 
 
-def save_frames_as_video(path, frames, fps):
+def save_frames_as_video(path, frames: typing.List[np.ndarray], fps):
     h, w = frames[0].shape[: 2]
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     writer = cv2.VideoWriter(path, fourcc, fps, (w, h))
@@ -10,3 +12,7 @@ def save_frames_as_video(path, frames, fps):
         writer.write(frame)
     writer.release()
     print(f'{path} saved')
+    print(f'Number of frames: {len(frames)}')
+    print(f'fps = {fps}')
+    if len(frames) > 0:
+        print(f'Resolution: {frames[0].shape}')

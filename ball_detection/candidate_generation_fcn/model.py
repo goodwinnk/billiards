d9c943cfadc1a93dc8f91058fef5131546ca929f
Andrey Gusev <andy.gusev@list.ru>
2020-05-23 01:36:06 +0300

    Update FCN for ball candidate generation
    Model size reduced. Trained with augmentations.
    Validation precision 0.433, recall 0.853

--- ball_detection/candidate_generation_fcn/model.py
+++ ball_detection/candidate_generation_fcn/model.py
@@ -5,17 +5,15 @@ class BallLocationFCN(nn.Module):
     def __init__(self):
         super().__init__()
         self.ops = nn.Sequential(
-            nn.Conv2d(3, 6, 5, padding=2),
+            nn.Conv2d(3, 5, 7, padding=3),
             nn.PReLU(),
-            nn.Conv2d(6, 6, 5, padding=2),
+            nn.Conv2d(5, 6, 7, padding=3),
             nn.PReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(6, 6, 7, padding=3),
+            nn.Conv2d(6, 5, 7, padding=3),
             nn.PReLU(),
-            nn.Conv2d(6, 6, 7, padding=3),
-            nn.PReLU(),
-            nn.Conv2d(6, 1, 5, padding=2),
+            nn.Conv2d(5, 1, 7, padding=3),
             nn.Sigmoid()
         )
 
